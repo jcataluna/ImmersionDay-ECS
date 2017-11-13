fb091150454936120f041aa9384b7017cefb6582
Jesús Cataluña Montejo <jcataluna@gmail.com>
2017-11-13 07:06:43 +0100

    Update index.php

--- www/index.php
+++ www/index.php
@@ -18,6 +18,7 @@
 <body>
 	<img id="logo" src="logo.png" />
 	<h1><?php echo "Hello ".($_ENV["NAME"]?$_ENV["NAME"]:"world")."!"; ?></h1>
+	<h2>Change it!</h2>
 	<?php if($_ENV["HOSTNAME"]) {?><h3>My hostname is <?php echo $_ENV["HOSTNAME"]; ?></h3><?php } ?>
 	<?php
 	$links = [];
